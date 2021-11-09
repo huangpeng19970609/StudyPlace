e38f5bffb8675fbd07c99cdf6c2489513be1b803
huangpeng <1215656702@qq.com>
2021-11-09 21:38:32 +0800

    123

--- 0_workSpaceVS/数据结构与算法/src/index.js
+++ 0_workSpaceVS/数据结构与算法/src/index.js
@@ -1,23 +1,20 @@
-/*
- * @Author: 黄鹏
- * @LastEditors: 黄鹏
- * @LastEditTime: 2021-10-31 23:29:49
- */
-function swap(array, a, b) {
-  // ES5特别的值之间的交换 => 
-  // 相当于 array[a] 与 array[b]的交换
-  [array[a], array[b]] = [array[b], array[a]]
-}
-function changeSort(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] > array[j]) {
-        swap(array, i, j)
-      }
+var removeDuplicates = function (nums) {
+  if (nums.length < 2) {
+    return nums.length
+  }
+
+  for (var i = 0, j = 1; i < nums.length; i++) {
+    console.log(i);
+    console.log(j);
+    console.log(nums);
+    if (nums[i + 1] > nums[i]) {
+      nums[j] = nums[i + 1]
+      j++
     }
   }
-  return array;
-}
+  return j;
+};
 
-let arr = bubleSort([-1, 9, 2, 3, -4, 100]);
+let arr = [0, 0, 1, 1, 2, 2, 3, 3];
+removeDuplicates(arr);
 console.log(arr);
