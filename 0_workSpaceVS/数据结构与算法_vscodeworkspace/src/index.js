1d4ab2e55a444a492f7503383137155ec645e2c3
huangpeng <huangpeng@piesat.cn>
2021-11-22 09:12:04 +0800

    钱钱钱

--- 0_workSpaceVS/数据结构与算法_vscodeworkspace/src/index.js
+++ 0_workSpaceVS/数据结构与算法_vscodeworkspace/src/index.js
@@ -1,12 +1,85 @@
 /*
  * @Author: 黄鹏
  * @LastEditors: 黄鹏
- * @LastEditTime: 2021-11-18 15:19:55
+ * @LastEditTime: 2021-11-19 16:38:14
  */
 
 // 标记顶点状态
+import { Queue } from "./lib/Queue";
+class Graph {
+  constructor() {
+    this.vertices = [];
+    this.adjList = new Map();
+  }
+  addVertex(v) {
+    if (!this.vertices.includes(v)) {
+      this.vertices.push(v);
+      this.adjList.set(v, []);
+      return v;
+    }
+    return false;
+  }
+  addEdge(v, w) {
+    if (!this.vertices.includes(v)) {
+      this.addVertex(v);
+    }
+    if (!this.vertices.includes(w)) {
+      this.addVertex(w);
+    }
+    this.adjList.get(v).push(w);
+  }
+  getVertices() {
+    return this.vertices;
+  }
+}
 
-import { Graph, BFS, getAllRouteByBFS, DFS } from './lib/Graph.js';
+const color = {
+  white: 0,
+  grey: 1,
+  black: 2,
+}
+function initColors(vertices) {
+  let colors = {};
+  vertices.forEach(item => {
+    colors[item] = color.white;
+  });
+  return colors;
+}
+function BFS(graph) {
+  let vertices = graph.getVertices();
+  let adjList = graph.adjList;
+  console.log(adjList);
+  let distance = {};
+  let predecessors = {};
+  let Q = new Queue();
+
+  vertices.map(v => {
+    distance[v] = 0;
+    predecessors[v] = null;
+  })
+  let colors = initColors(vertices);
+  Q.enqueue(vertices[0]);
+  while (!Q.isEmpty()) {
+    const u = Q.dequeue();
+    const neighbor = adjList.get(u);
+    console.log(neighbor);
+    for (let i = 0; i < neighbor.length; i++) {
+      let v = neighbor[i];
+      if (colors[v] === color.white) {
+        Q.enqueue(v);
+        colors[v] = color.grey;
+        distance[v] = distance[u] + 1;
+        predecessors[v] = u;
+      }
+    }
+    colors[u] = color.black;
+  }
+  return {
+    distance,
+    predecessors
+  }
+}
+
 
 
 
@@ -29,17 +102,6 @@ edge.map(item => {
   graph.addEdge(item[0], item[1]);
 });
 
-console.log(graph);
-console.log(graph.toString());
 
 let bfs = BFS(graph, graph.getVertices()[0])
 console.log(bfs);
-getAllRouteByBFS(
-  graph.getVertices(),
-  bfs.predecessors
-)
-
-DFS(
-  graph,
-  (value) => { console.log(value) }
-)
---
+++ 数据结构/排序算法/fastSearchSort.js
@@ -0,0 +1,6 @@
+function quickSort(arr) {
+  return quick(arr, 0, arr.length - 1);
+}
+function quick(arr, left, right) {
+  let pivot = 
+}
